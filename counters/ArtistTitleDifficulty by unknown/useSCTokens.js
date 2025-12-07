29262ce6910311c12e72a359da79f96667dc84e1
DARK-ECNELIS <darkecnelis@gmail.com>
2025-12-07 08:47:11 -0400

    delete

--- counters/ArtistTitleDifficulty by unknown/useSCTokens.js
+++
@@ -1,9 +0,0 @@
-//provides access to: rws - websocket client; tokens - object with current tokens; getToken - method for getting token values
-const useSCTokens = (requiredTokensArray) => {
-  let tokensObj = Vue.inject('SCTokens');
-  tokensObj.rws.AddTokens(requiredTokensArray || []);
-
-  return tokensObj;
-};
-
-export { useSCTokens };
